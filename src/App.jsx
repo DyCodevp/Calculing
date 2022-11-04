4797b08857b5867b07fe701d3a043021fe1ccf2d
Dylan Chaves <Dylanpotoy@gmail.com>
2022-11-04 16:23:45 -0600

    this commit is just for add a new funtion of butto

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,24 @@
 import { useState } from "react";
 import "./App.css";
 
-// function ShowALL(Number) {
-//   const { total, setTotal } = useState("0");
-
-//   if (Number === total) return setTotal((total) => total + Number);
-//   return (
-//     <div className="Cointeiner-bar">
-//       <span id="bar">{Number}</span>
-//     </div>
-//   );
-// }
-
-// function Operations(actualNumber) {
-//   let total =
-//     actualNumber.number1 + actualNumber.operation + actualNumber.number2;
-//   total = total.toString();
-//   result = eval(total);
-//   return result;
-// }
-
-console.log(eval("1 +  2 + 3"));
-console.log(eval("1 +  2 + 3"));
 function Buttons() {
   return (
     <div className="buttonsRoot">
-      <button type="text" className="buttons">
-        1
-      </button>
-      <button type="text" className="buttons">
-        2
-      </button>
-      <button type="text" className="buttons">
-        3
-      </button>
-      <button type="text" className="buttons">
-        4
-      </button>
-      <button type="text" className="buttons">
-        5
-      </button>
-      <button type="text" className="buttons">
-        6
-      </button>
-      <button type="text" className="buttons">
-        7
-      </button>
-      <button type="text" className="buttons">
-        8
-      </button>
-      <button type="text" className="buttons">
-        9
-      </button>
+      <CreateButton value={1} className="buttons" />
+      <CreateButton value={2} className="buttons" />
+      <CreateButton value={3} className="buttons" />
+      <CreateButton value={4} className="buttons" />
+      <CreateButton value={5} className="buttons" />
+      <CreateButton value={6} className="buttons" />
+      <CreateButton value={7} className="buttons" />
+      <CreateButton value={8} className="buttons" />
+      <CreateButton value={9} className="buttons" />
+      <CreateButton value={0} className="buttons" />
     </div>
   );
 }
-function SpecialButtons() {
-  return (
-    <div className="specialButtonRoot">
-      <button type="text" className="buttonSpecial">
-        +
-      </button>
-      <button type="text" className="buttonSpecial">
-        -
-      </button>
-      <button type="text" className="buttonSpecial">
-        .
-      </button>
-      <button type="text" className="buttonSpecial">
-        =
-      </button>
-      <button type="text" className="buttonSpecial">
-        *
-      </button>
-      <button type="text" className="buttonSpecial">
-        /
-      </button>
-      <button type="text" className="buttonSpecial">
-        C
-      </button>
-    </div>
-  );
+function CreateButton(props) {
+  return <button className={props.className}>{props.value}</button>;
 }
 
 function App() {
@@ -88,7 +26,6 @@ function App() {
     <div className="App">
       <h1>Calculator</h1>
       <Buttons />
-      <SpecialButtons />
     </div>
   );
 }
