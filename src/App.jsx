dea3df832c1e6f5573af218c2d0f1188beda1988
Dylan Chaves <Dylanpotoy@gmail.com>
2022-11-04 19:21:41 -0600

    the calculator is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,84 @@
 import { useState } from "react";
 import "./App.css";
 
-function Buttons() {
+function Calculator() {
+  const [display, setDisplay] = useState(0);
+  function handleNumber(value) {
+    setDisplay(`${display}` + value);
+  }
+  function handleOperator(value) {
+    setDisplay(display + value);
+  }
+  function handleTotal() {
+    let evaluation = eval(display);
+    setDisplay(evaluation);
+  }
+
   return (
     <div className="buttonsRoot">
-      <CreateButton value={1} className="buttons" />
-      <CreateButton value={2} className="buttons" />
-      <CreateButton value={3} className="buttons" />
-      <CreateButton value={4} className="buttons" />
-      <CreateButton value={5} className="buttons" />
-      <CreateButton value={6} className="buttons" />
-      <CreateButton value={7} className="buttons" />
-      <CreateButton value={8} className="buttons" />
-      <CreateButton value={9} className="buttons" />
-      <CreateButton value={0} className="buttons" />
+      <div className="display">{display}</div>
+      <CreateButton onClick={handleNumber} value={7} className="buttons" />
+      <CreateButton onClick={handleNumber} value={8} className="buttons" />
+      <CreateButton onClick={handleNumber} value={9} className="buttons" />
+      <CreateButton
+        onClick={handleOperator}
+        value={"*"}
+        className="operatorButtons"
+      />
+
+      <CreateButton onClick={handleNumber} value={4} className="buttons" />
+      <CreateButton onClick={handleNumber} value={5} className="buttons" />
+      <CreateButton onClick={handleNumber} value={6} className="buttons" />
+      <CreateButton
+        onClick={handleOperator}
+        value={"-"}
+        className="operatorButtons"
+      />
+
+      <CreateButton onClick={handleNumber} value={1} className="buttons" />
+      <CreateButton onClick={handleNumber} value={2} className="buttons" />
+      <CreateButton onClick={handleNumber} value={3} className="buttons" />
+      <CreateButton
+        onClick={handleOperator}
+        value={"+"}
+        className="operatorButtons"
+      />
+
+      <CreateButton
+        onClick={handleOperator}
+        value={"C"}
+        className="operatorButtons"
+      />
+      <CreateButton onClick={handleNumber} value={0} className="buttons" />
+      <CreateButton
+        onClick={handleOperator}
+        value={"."}
+        className="operatorButtons"
+      />
+      <CreateButton
+        onClick={handleTotal}
+        value={"="}
+        className="operatorButtons"
+      />
     </div>
   );
 }
 function CreateButton(props) {
-  return <button className={props.className}>{props.value}</button>;
+  return (
+    <button
+      onClick={() => props.onClick(props.value)}
+      className={props.className}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 function App() {
   return (
-    <div className="App">
+    <div className="App-container">
       <h1>Calculator</h1>
-      <Buttons />
+      <Calculator />
     </div>
   );
 }
